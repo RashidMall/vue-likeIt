3356662e7bb69b25a9ace35bd11947428b5343f7
RashidMall <mr.mallaev@gmail.com>
2018-10-23 16:15:49 +0200

    Put card and upvote method into new component

--- app/main.js
+++ app/main.js
@@ -1,3 +1,36 @@
+const submissionComponent = {
+    template:
+    `<div>
+        <img class="card-img-top" v-bind:src="submission.submissionImage" alt="Card image cap">
+        <div class="card-body">
+            <div class="d-inline-block float-right">
+                <span class="icon is-small" v-on:click="upvote(submission.id)">
+                    <i class="fa fa-chevron-up"></i>
+                    <strong class="has-text-info">{{ submission.votes }}</strong>
+                </span>
+            </div>
+            <h5 class="card-title d-inline-block">
+                <a v-bind:href="submission.url" class="has-text-info">{{ submission.title }}</a>
+                <span class="badge badge-pill badge-primary">#{{ submission.id }}</span>
+            </h5>
+            <p class="card-text">{{ submission.description }}</p>
+            <small class="w-25">
+                Submitted by:
+                <img v-bind:src="submission.avatar" alt="daniel's avatar" class="img-fluid avatar">
+            </small>
+        </div>
+    </div>`,
+    props: ['submission', 'submissions'],
+    methods: {
+        upvote(submissionId) {
+            const submission = this.submissions.find(
+                submission => submission.id === submissionId
+            );
+            submission.votes++;
+        }
+    }
+};
+
 new Vue({
     el: '#app',
     data: {
@@ -17,5 +50,8 @@ new Vue({
             );
             submission.votes++;
         }
+    },
+    components: {
+        'submission-component': submissionComponent
     }
 });
