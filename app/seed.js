f5c0f8065343ac4bd67c96d26ece60ac568b1edf
RashidMall <mr.mallaev@gmail.com>
2018-10-23 11:48:37 +0200

    Create sample data for submissions

---
+++ app/seed.js
@@ -0,0 +1,42 @@
+window.Seed = (function () {
+    const submissions = [
+      {
+        id: 1,
+        title: 'Forecasters call for weather on Monday',
+        description: "They're hesitant to predict snowstorm here.",
+        url: '#',
+        votes: 158,
+        avatar: '../public/images/avatars/daniel.jpg',
+        submissionImage: '../public/images/submissions/yellow.png',
+      },
+      {
+        id: 2,
+        title: 'Amphibious pitcher makes debut',
+        description: 'Venditte becomes first pitcher in 20 years to pitch with both arms in MLB game.',
+        url: '#',
+        votes: 110,
+        avatar: '../public/images/avatars/kristy.png',
+        submissionImage: '../public/images/submissions/rose.png',
+      },
+      {
+        id: 3,
+        title: 'Cows lose their jobs as milk prices drop',
+        description: "It's always heartbreaking when a cow can't bring in a steady income.",
+        url: '#',
+        votes: 117,
+        avatar: '../public/images/avatars/veronika.jpg',
+        submissionImage: '../public/images/submissions/steel.png',
+      },
+      {
+        id: 4,
+        title: 'Miracle cure kills fifth patient',
+        description: 'You may want to look up the definition of the word "cure".',
+        url: '#',
+        votes: 19,
+        avatar: '../public/images/avatars/molly.png',
+        submissionImage: '../public/images/submissions/aqua.png',
+      }
+    ];
+  
+    return { submissions: submissions };
+  }());
